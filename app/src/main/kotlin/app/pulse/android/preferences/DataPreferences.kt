c2d035ddefe96e92c1f62392fa984a9c8cc23367
huza <huzaifahasif3@gmail.com>
2026-04-23 22:07:46 +0700

    increase cache size limit

--- app/src/main/kotlin/app/pulse/android/preferences/DataPreferences.kt
+++ app/src/main/kotlin/app/pulse/android/preferences/DataPreferences.kt
@@ -12,8 +12,9 @@ import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 
 object DataPreferences : GlobalPreferencesHolder() {
-    var coilDiskCacheMaxSize by enum(CoilDiskCacheSize.`128MB`)
-    var exoPlayerDiskCacheMaxSize by enum(ExoPlayerDiskCacheSize.`2GB`)
+    var coilDiskCacheMaxSize by enum(CoilDiskCacheSize.`2GB`)
+    var exoPlayerDiskCacheMaxSize by enum(ExoPlayerDiskCacheSize.`4GB`)
+
     var pauseHistory by boolean(false)
     var pausePlaytime by boolean(false)
     var pauseSearchHistory by boolean(false)
