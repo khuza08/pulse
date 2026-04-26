24f4c874ec198a87ae26ad812e741646fa51e331
huza <huzaifahasif3@gmail.com>
2026-04-26 10:58:32 +0700

    507cd05

--- app/src/main/kotlin/app/pulse/android/service/PlayerService.kt
+++ app/src/main/kotlin/app/pulse/android/service/PlayerService.kt
@@ -982,7 +982,7 @@ class PlayerService : InvincibleService(), Player.Listener, PlaybackStatsListene
                 .setOnlyAlertOnce(true)
                 .setShowWhen(false)
                 .setSmallIcon(
-                    player.playerError?.let { R.drawable.alert_circle } ?: R.drawable.app_icon
+                    player.playerError?.let { R.drawable.alert_circle } ?: R.drawable.ic_launcher_monochrome
                 )
                 .setOngoing(false)
                 .setContentIntent(
